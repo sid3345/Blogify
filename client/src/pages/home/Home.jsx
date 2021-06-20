7083b3ed54db3a04d57adc7d79816301d75f11c2
Siddharth Sinha <sid3345@gmail.com>
2021-06-21 02:12:10 +0530

    removed sidebar

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
-import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
 import axios from "axios";
 import { useLocation } from "react-router";
@@ -22,7 +21,6 @@ export default function Home() {
       <Header />
       <div className="home">
         <Posts posts={posts} />
-        <Sidebar />
       </div>
     </>
   );
