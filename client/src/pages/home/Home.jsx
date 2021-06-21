6cf4a2d947b9cd93c062ea255c5bd87c3bbe98c3
Siddharth Sinha <sid3345@gmail.com>
2021-06-22 02:02:40 +0530

    added search functionality

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,10 +4,35 @@ import Posts from "../../components/posts/Posts";
 import "./home.css";
 import axios from "axios";
 import { useLocation } from "react-router";
+import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from "@material-ui/core/styles";
+import InputBase from '@material-ui/core/InputBase';
+
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: 'sticky',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+   
+}))
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
+
+  const classes = useStyles();
+
+  const [keyword, setKeyword] = useState('');
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -16,11 +41,32 @@ export default function Home() {
     };
     fetchPosts();
   }, [search]);
+
+  const handleSearchChange=(e)=>{
+    setKeyword(e.target.value.toLowerCase());
+    }
+
+  const filteredPosts = posts.filter((post)=>{
+  let postTitle = post.title.toString().toLowerCase();
+  return postTitle.indexOf(keyword) > -1;
+  })
+
   return (
     <>
       <Header />
+            <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+            
+              inputProps={{ 'aria-label': 'search', 
+              onChange: (e)=>handleSearchChange(e) }}
+            />
+          </div>
       <div className="home">
-        <Posts posts={posts} />
+        <Posts posts={filteredPosts} />
       </div>
     </>
   );
