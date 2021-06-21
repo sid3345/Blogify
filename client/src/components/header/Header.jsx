a91446c613536e9c69d9e02fc41eb593ede2efe0
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-06-21 16:38:15 +0530

    desing complete

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
+
       </div>
       <img
         className="headerImg"
